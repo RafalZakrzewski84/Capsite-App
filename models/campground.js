73649fb8bf3f8ff6146ad960f5601f2100ef8920
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-06-09 14:54:56 +0200

    Creating schema and exporting mongoose model.

---
+++ models/campground.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+//add schema to variable
+const Schema = mongoose.Schema;
+
+//setting new schema
+const CampgroundSchema = new Schema({
+    title: String,
+    price: String,
+    description: String,
+    location: String
+});
+
+//setting and exporting mongoose model
+module.exports = mongosose.model('Campground', CampgroundSchema);
