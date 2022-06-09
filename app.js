3b10dc7506d895da8e8d966d5ccb27efbbe3bf59
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-06-09 14:44:10 +0200

    Setting view engine to ejs and create view folder.

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
+
+
+app.use('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req,res) => {
     res.send('hello word')
