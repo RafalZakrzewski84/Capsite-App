e8c5eb0851ebecd7651060feb0975029df83c273
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-06-20 10:39:16 +0200

    generating camps data with new schema

--- seeds/index.js
+++ seeds/index.js
@@ -1,3 +1,5 @@
+//In terminal use commend node index.js or node seeds/index.js if you are outside seeds folder
+
 const mongoose = require('mongoose');
 const cities = require('./cities');
 const {descriptors, places} = require('./seedHelpers');
@@ -28,11 +30,15 @@ const seedDB = async () => {
     //loop for generating new document in mongo db
     for (let i=0; i <50; i++){
         const random1000 = Math.floor(Math.random()*1000);
-
+        const price = Math.floor(Math.random() * 30) + 10;
+ 
         //new camp with name and location
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251/',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit eaque esse illo veritatis rem consequatur velit, magni impedit aperiam! Quam nulla deserunt impedit et illum dolorum beatae dolores accusantium a?',
+            price
         })
         await camp.save();
     }
