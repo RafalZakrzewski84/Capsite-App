9e5dd889ce6d4bff17ea3de0e86f69069cdef251
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-06-30 09:38:45 +0200

    adding review schema and connecting with campgrounds

--- models/campground.js
+++ models/campground.js
@@ -3,13 +3,19 @@ const mongoose = require('mongoose');
 //add schema to variable
 const Schema = mongoose.Schema;
 
-//setting new schema
+//setting new schema for Campgrounds
 const CampgroundSchema = new Schema({
-    title: String,
-    image: String,
-    price: Number,
-    description: String,
-    location: String
+	title: String,
+	image: String,
+	price: Number,
+	description: String,
+	location: String,
+	reviews: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'Review',
+		},
+	],
 });
 
 //setting and exporting mongoose model
---
+++ models/review.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+//add schema to variable
+const Schema = mongoose.Schema;
+
+//setting new schema for reviews
+const reviewSchema = new Schema({
+	body: String,
+	rating: Number,
+});
+
+module.exports = mongoose.model('Review', reviewSchema);
