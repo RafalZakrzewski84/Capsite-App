3854f0bc1fd5ed8c14b269a7c2c62e1b8efd06b4
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-06 10:05:43 +0200

    correcting typo

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 //using public directory with js files
-app.use(exspress.status(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //app middleware
 //for parsing data form page forms
