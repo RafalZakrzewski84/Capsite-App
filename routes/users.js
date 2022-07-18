2d0e6a5c354bc9f4208face2a61e77edc259cd0e
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-18 09:53:07 +0200

    adding post route for adding new user to db

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,10 @@
 const express = require('express');
 const router = express.Router();
 
+//adding error class - catchAsync is wrapping every async function in code below
+const catchAsync = require('../utils/catchAsync');
+const ExpressError = require('../utils/ExpressError');
+
 //adding users schema
 const User = require('../models/users');
 
@@ -10,5 +14,13 @@ const User = require('../models/users');
 router.get('/register', (req, res) => {
 	res.render('users/register');
 });
+//adding data from register form to db - new user
+router.post(
+	'/register',
+	catchAsync(async (req, res, next) => {
+		const user = new User(req.body.register);
+		res.send(user);
+	})
+);
 
 module.exports = router;
