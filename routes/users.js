d2e712f2e42e41c4121793ab6b87d90f8ed474e6
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-18 11:38:05 +0200

    correcting error in logout function

--- routes/users.js
+++ routes/users.js
@@ -58,10 +58,14 @@ router.post(
 	}
 );
 
-router.get('/logout', (req, res) => {
-	req.logout();
-	req.flash('success', 'Goodbye ');
-	res.redirect('/campgrounds');
+router.get('/logout', (req, res, next) => {
+	req.logout(function (err) {
+		if (err) {
+			return next(err);
+		}
+		req.flash('success', 'Goodbye');
+		res.redirect('/campgrounds');
+	});
 });
 
 module.exports = router;
