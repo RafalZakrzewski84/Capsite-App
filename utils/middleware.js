2f87ded3596478edce2c2f13dad8cd31b2222565
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-18 12:26:59 +0200

    adding returnTo path

--- utils/middleware.js
+++ utils/middleware.js
@@ -2,6 +2,9 @@
 
 module.exports.isLoggedIn = (req, res, next) => {
 	if (!req.isAuthenticated()) {
+		//remembering path before redirecting to login in session
+		req.session.returnTo = req.originalUrl;
+
 		req.flash('error', 'You must be signed in first');
 		return res.redirect('/login');
 	}
