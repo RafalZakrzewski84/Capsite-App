62359dd2bc131a7d4fa61573bc683c669cd85112
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-08-16 11:45:53 +0200

    moving rest of functions from routes to controller

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -2,10 +2,90 @@
 //importing mongoose schema for campgrounds
 const Campground = require('../models/campground');
 
-module.exports.index = async (req, res) => {
+//modules are imported in /routes/campgrouds.js
+
+module.exports.renderCampListPage = async (req, res) => {
 	//finding all campground in db
 	const campgrounds = await Campground.find({});
 
 	//render page with all campgrounds
 	res.render('campgrounds/index', { campgrounds });
+};
+
+module.exports.renderNewCampForm = (req, res) => {
+	res.render('campgrounds/new');
+};
+
+module.exports.createNewCamp = async (req, res, next) => {
+	//new campground with data from from
+	const camp = new Campground(req.body.campground);
+
+	//assigning logged in user as a creator of campground (req.user added by passport.authenticate() in routes users.js)
+	camp.author = req.user._id;
+
+	//saving new camp to db
+	await camp.save();
+
+	//flash msq
+	req.flash('success', 'New Campground Added');
+	res.redirect(`/campgrounds/${camp._id}`);
+};
+
+module.exports.renderCampDetailsPage = async (req, res) => {
+	const { id } = req.params;
+
+	//finding campground in db by it id - id come from url (req.params)
+	//populating reviews data and author
+	const camp = await Campground.findById(id)
+		.populate({
+			path: 'reviews',
+			populate: {
+				path: 'author',
+			},
+		})
+		.populate('author');
+
+	//show flash error msg when campground not found
+	if (!camp) {
+		req.flash('error', 'Campground Not Found');
+		return res.redirect('/campgrounds');
+	}
+
+	//rendering page with camp details
+	res.render('campgrounds/show', { camp });
+};
+
+module.exports.renderCampEditForm = async (req, res) => {
+	const { id } = req.params;
+	//opening camp page by camp id
+	const camp = await Campground.findById(id);
+
+	//show flash error msg when campground not found
+	if (!camp) {
+		req.flash('error', 'Campground Not Found');
+		return res.redirect('/campgrounds');
+	}
+	res.render('campgrounds/edit', { camp });
+};
+
+module.exports.editCamp = async (req, res) => {
+	//finding and updating camp with new data from edit form
+	const camp = await Campground.findByIdAndUpdate(id, {
+		...req.body.campground,
+	});
+
+	//flash msq
+	req.flash('success', 'Campground Edited Successfully');
+	res.redirect(`/campgrounds/${camp._id}`);
+};
+
+module.exports.deleteCamp = async (req, res) => {
+	//camp by id
+	const { id } = req.params;
+	//find and delete from db
+	const camp = await Campground.findByIdAndDelete(id);
+
+	//flash msq
+	req.flash('success', 'Campground Successfully Deleted');
+	res.redirect('/campgrounds');
 };
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -21,78 +21,27 @@ const {
 
 //BASIC ROUTE
 //list of all campgrounds
-router.get('/', catchAsync(campgrounds.index));
+router.get('/', catchAsync(campgrounds.renderCampListPage));
 
 //page for adding new campground (before :id to prevent triggering .findById('new'))
-router.get('/new', isLoggedIn, (req, res) => {
-	res.render('campgrounds/new');
-});
+router.get('/new', isLoggedIn, campgrounds.renderNewCampForm);
 //data from form - new camp page
 router.post(
 	'/',
 	isLoggedIn,
 	validateCampground,
-	catchAsync(async (req, res, next) => {
-		//new campground with data from from
-		const camp = new Campground(req.body.campground);
-
-		//assigning logged in user as a creator of campground (req.user added by passport.authenticate() in routes users.js)
-		camp.author = req.user._id;
-
-		//saving new camp to db
-		await camp.save();
-
-		//flash msq
-		req.flash('success', 'New Campground Added');
-		res.redirect(`/campgrounds/${camp._id}`);
-	})
+	catchAsync(campgrounds.createNewCamp)
 );
 
 //page with detailed information about comp
-router.get(
-	'/:id',
-	catchAsync(async (req, res) => {
-		const { id } = req.params;
-
-		//finding campground in db by it id - id come from url (req.params)
-		//populating reviews data and author
-		const camp = await Campground.findById(id)
-			.populate({
-				path: 'reviews',
-				populate: {
-					path: 'author',
-				},
-			})
-			.populate('author');
-		console.log(camp);
-
-		//show flash error msg when campground not found
-		if (!camp) {
-			req.flash('error', 'Campground Not Found');
-			return res.redirect('/campgrounds');
-		}
-		//rendering page with camp details
-		res.render('campgrounds/show', { camp });
-	})
-);
+router.get('/:id', catchAsync(campgrounds.renderCampDetailsPage));
 
 //editing existing camp when log in and you are author
 router.get(
 	'/:id/edit',
 	isLoggedIn,
 	isAuthor,
-	catchAsync(async (req, res) => {
-		const { id } = req.params;
-		//opening camp page by camp id
-		const camp = await Campground.findById(id);
-
-		//show flash error msg when campground not found
-		if (!camp) {
-			req.flash('error', 'Campground Not Found');
-			return res.redirect('/campgrounds');
-		}
-		res.render('campgrounds/edit', { camp });
-	})
+	catchAsync(campgrounds.renderCampEditForm)
 );
 //updating particular camp and show details when log in and you are author
 router.put(
@@ -100,32 +49,10 @@ router.put(
 	isLoggedIn,
 	isAuthor,
 	validateCampground,
-	catchAsync(async (req, res) => {
-		//finding and updating camp with new data from edit form
-		const camp = await Campground.findByIdAndUpdate(id, {
-			...req.body.campground,
-		});
-
-		//flash msq
-		req.flash('success', 'Campground Edited Successfully');
-		res.redirect(`/campgrounds/${camp._id}`);
-	})
+	catchAsync(campgrounds.editCamp)
 );
 
 //deleting camp
-router.delete(
-	'/:id',
-	isLoggedIn,
-	catchAsync(async (req, res) => {
-		//camp by id
-		const { id } = req.params;
-		//find and delete from db
-		const camp = await Campground.findByIdAndDelete(id);
-
-		//flash msq
-		req.flash('success', 'Campground Successfully Deleted');
-		res.redirect('/campgrounds');
-	})
-);
+router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCamp));
 
 module.exports = router;
