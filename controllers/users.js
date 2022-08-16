01466cdbbd62cda33c12f5823600d3fdd9329607
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-08-16 12:12:26 +0200

    moving functions from users routes to controller

---
+++ controllers/users.js
@@ -0,0 +1,59 @@
+/** @format */
+
+//adding passport package
+const passport = require('passport');
+//adding users schema
+const User = require('../models/users');
+
+module.exports.renderRegisterForm = (req, res) => {
+	res.render('users/register');
+};
+
+module.exports.registerUser = async (req, res, next) => {
+	try {
+		//data from register form
+		const { username, email, password } = req.body;
+		//creating new user
+		const user = new User({ username, email });
+		//registering new user by passport middleware
+		const registeredUser = await User.register(user, password);
+		// console.log(registeredUser);
+
+		//automatic login after registering new user
+		req.login(registeredUser, function (err) {
+			if (err) {
+				return next(err);
+			}
+			//greetings of new user
+			req.flash('success', `Welcome ${username} in Yelp-Camp`);
+			res.redirect('/campgrounds');
+		});
+	} catch (e) {
+		//msg if registering process gone wrong
+		req.flash('error', e.message);
+		res.redirect('/register');
+	}
+};
+
+module.exports.renderLoginForm = (req, res) => {
+	res.render('users/login');
+};
+
+module.exports.loginUser = (req, res) => {
+	req.flash('success', 'Welcome back');
+
+	//path to redirect user to page from before logging
+	const redirectUrl = req.session.returnTo || '/campgrounds';
+	delete req.session.returnTo;
+	res.redirect(redirectUrl);
+};
+
+module.exports.logoutUser = (req, res, next) => {
+	req.logout(function (err) {
+		if (err) {
+			return next(err);
+		}
+		req.flash('success', 'Goodbye');
+		res.redirect('/campgrounds');
+	});
+};
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,9 @@
 const express = require('express');
 const router = express.Router();
 
+//importing users controller
+const users = require('../controllers/users');
+
 //adding passport package
 const passport = require('passport');
 
@@ -14,43 +17,12 @@ const ExpressError = require('../utils/ExpressError');
 const User = require('../models/users');
 
 //setting register route
-router.get('/register', (req, res) => {
-	res.render('users/register');
-});
+router.get('/register', users.renderRegisterForm);
 //adding data from register form to db - new user
-router.post(
-	'/register',
-	catchAsync(async (req, res, next) => {
-		try {
-			//data from register form
-			const { username, email, password } = req.body;
-			//creating new user
-			const user = new User({ username, email });
-			//registering new user by passport middleware
-			const registeredUser = await User.register(user, password);
-			// console.log(registeredUser);
-
-			//automatic login after registering new user
-			req.login(registeredUser, function (err) {
-				if (err) {
-					return next(err);
-				}
-				//greetings of new user
-				req.flash('success', `Welcome ${username} in Yelp-Camp`);
-				res.redirect('/campgrounds');
-			});
-		} catch (e) {
-			//msg if registering process gone wrong
-			req.flash('error', e.message);
-			res.redirect('/register');
-		}
-	})
-);
+router.post('/register', catchAsync(users.registerUser));
 
 //setting login route
-router.get('/login', (req, res) => {
-	res.render('users/login');
-});
+router.get('/login', users.renderLoginForm);
 //adding login logic - using passport method for auth
 router.post(
 	'/login',
@@ -59,24 +31,9 @@ router.post(
 		failureRedirect: '/login',
 		keepSessionInfo: true,
 	}),
-	(req, res) => {
-		req.flash('success', 'Welcome back');
-
-		//path to redirect user to page from before logging
-		const redirectUrl = req.session.returnTo || '/campgrounds';
-		delete req.session.returnTo;
-		res.redirect(redirectUrl);
-	}
+	users.loginUser
 );
 
-router.get('/logout', (req, res, next) => {
-	req.logout(function (err) {
-		if (err) {
-			return next(err);
-		}
-		req.flash('success', 'Goodbye');
-		res.redirect('/campgrounds');
-	});
-});
+router.get('/logout', users.logoutUser);
 
 module.exports = router;
