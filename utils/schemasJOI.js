2a0f5cc1498aa9d76485c3fd7b9638594ff382b1
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-08-17 14:31:02 +0200

    udating validation schema for campgrounds

--- utils/schemasJOI.js
+++ utils/schemasJOI.js
@@ -1,10 +1,12 @@
+/** @format */
+
 const Joi = require('joi');
 
 module.exports.campgroundJoiSchema = Joi.object({
 	campground: Joi.object({
 		title: Joi.string().required(),
 		location: Joi.string().required(),
-		image: Joi.string().required(),
+		// image: Joi.string().required(),
 		price: Joi.number().min(1).required(),
 		description: Joi.string().required(),
 	}).required(),
