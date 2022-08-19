2ace8026ff1a438b1e8da0c3103e6a378ccdfc6e
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-08-19 15:13:19 +0200

    styling cluster map

--- public/javascripts/indexPageClusterMap.js
+++ public/javascripts/indexPageClusterMap.js
@@ -3,7 +3,7 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
 	container: 'map',
-	style: 'mapbox://styles/mapbox/dark-v10',
+	style: 'mapbox://styles/mapbox/light-v10',
 	center: [-103.5917, 40.6699],
 	zoom: 3,
 });
@@ -36,13 +36,13 @@ map.on('load', () => {
 			'circle-color': [
 				'step',
 				['get', 'point_count'],
-				'#51bbd6',
-				100,
-				'#f1f075',
-				750,
-				'#f28cb1',
+				'#00bcd4',
+				10,
+				'#2196f3',
+				30,
+				'#3f51b5',
 			],
-			'circle-radius': ['step', ['get', 'point_count'], 20, 100, 30, 750, 40],
+			'circle-radius': ['step', ['get', 'point_count'], 15, 10, 20, 30, 25],
 		},
 	});
 
