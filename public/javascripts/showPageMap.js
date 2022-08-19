dbb8efd1b81c24d48628d84da5bc51acdd6a1e6a
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-08-19 11:51:16 +0200

    setting fog on map

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -9,5 +9,11 @@ const map = new mapboxgl.Map({
 	projection: 'globe', // display the map as a 3D globe
 });
 map.on('style.load', () => {
-	map.setFog({}); // Set the default atmosphere style
+	map.setFog({
+		color: 'rgb(186, 210, 235)', // Lower atmosphere
+		'high-color': 'rgb(36, 92, 223)', // Upper atmosphere
+		'horizon-blend': 0.02, // Atmosphere thickness (default 0.2 at low zooms)
+		'space-color': 'rgb(11, 11, 25)', // Background color
+		'star-intensity': 0.6, // Background star brightness (default 0.35 at low zoooms )
+	});
 });
