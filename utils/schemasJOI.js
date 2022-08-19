1eb9b3f3da00b5ed23900ba2cdc5d079b06d83a3
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-08-19 23:43:46 +0200

    adding basic XSS protection in Joi schema validation

--- utils/schemasJOI.js
+++ utils/schemasJOI.js
@@ -1,14 +1,36 @@
 /** @format */
 
-const Joi = require('joi');
+const baseJoi = require('joi');
+const sanitizeHtml = require('sanitize-html');
+
+const extension = (joi) => ({
+	type: 'million',
+	base: joi.string(),
+	messages: { 'string.escapeHTML': '{{#label}} must not be include HTML' },
+	rules: {
+		escapeHTML: {
+			validate(value, helpers) {
+				const clean = sanitizeHtml(value, {
+					allowedTags: [],
+					allowedAttributes: {},
+				});
+				if (clean !== value)
+					return helpers.error('string.escapeHTML', { value });
+				return clean;
+			},
+		},
+	},
+});
+
+const Joi = baseJoi.extend(extension);
 
 module.exports.campgroundJoiSchema = Joi.object({
 	campground: Joi.object({
-		title: Joi.string().required(),
-		location: Joi.string().required(),
+		title: Joi.string().required().escapeHTML(),
+		location: Joi.string().required().escapeHTML(),
 		// image: Joi.string().required(),
 		price: Joi.number().min(1).required(),
-		description: Joi.string().required(),
+		description: Joi.string().required().escapeHTML(),
 	}).required(),
 	deleteImages: Joi.array(),
 });
@@ -16,6 +38,6 @@ module.exports.campgroundJoiSchema = Joi.object({
 module.exports.reviewJoiSchema = Joi.object({
 	review: Joi.object({
 		rating: Joi.number().min(1).max(5).required(),
-		body: Joi.string().required(),
+		body: Joi.string().required().escapeHTML(),
 	}).required(),
 });
