b87c6fb3986e758dce8ae2a54496ffc066cc5c05
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-08-21 16:55:51 +0200

    styling Camp App

--- public/javascripts/indexPageClusterMap.js
+++ public/javascripts/indexPageClusterMap.js
@@ -3,7 +3,7 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
 	container: 'cluster-map',
-	style: 'mapbox://styles/mapbox/light-v10',
+	style: 'mapbox://styles/mapbox/dark-v10',
 	center: [-103.5917, 40.6699],
 	zoom: 3,
 });
