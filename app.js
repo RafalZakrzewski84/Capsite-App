fe41a3979408c02207dc483213229db3a5b109ad
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-08-21 17:08:58 +0200

    fixing bugs

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ app.use(flash());
 app.use(helmet());
 
 const scriptSrcUrls = [
-	'https://stackpath.bootstrapcdn.com/',
+	'https://cdn.jsdelivr.net/',
 	'https://api.tiles.mapbox.com/',
 	'https://api.mapbox.com/',
 	'https://kit.fontawesome.com/',
@@ -103,7 +103,7 @@ const scriptSrcUrls = [
 ];
 const styleSrcUrls = [
 	'https://kit-free.fontawesome.com/',
-	'https://stackpath.bootstrapcdn.com/',
+	'https://cdn.jsdelivr.net/',
 	'https://api.mapbox.com/',
 	'https://api.tiles.mapbox.com/',
 	'https://fonts.googleapis.com/',
