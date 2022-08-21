e0433bf0ee8846bd04525732c585d6ebf9d6ac0d
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-08-21 22:34:17 +0200

    adding start script and port variable

--- app.js
+++ app.js
@@ -196,6 +196,8 @@ app.use((err, req, res, next) => {
 	res.status(statusCode).render('error', { err });
 });
 
-app.listen(3000, () => {
-	console.log('App is listening on 3000 port.');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+	console.log(`App is listening on ${port} port.`);
 });
