9660826099595e98101fd01f58d278e119834a15
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-08-21 17:34:47 +0200

    fixing bugs

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -3,7 +3,7 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
 	container: 'map', // container ID
-	style: 'mapbox://styles/mapbox/dark-v10', // style URL
+	style: 'mapbox://styles/mapbox/streets-v11', // style URL
 	center: campground.geometry.coordinates, // starting position [lng, lat]
 	zoom: 6, // starting zoom
 	projection: 'globe', // display the map as a 3D globe
